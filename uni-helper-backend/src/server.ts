609838ea78976a23f6d522fbb02cd6b5e2b7fe45
Balint Dolla <balint.dolla@realcity.io>
2021-09-23 14:44:47 +0200

    Create deployment flow

--- uni-helper-backend/src/server.ts
+++ uni-helper-backend/src/server.ts
@@ -1,20 +1,22 @@
-import express, { RequestHandler } from 'express';
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 import compression from 'compression';
-import cors from 'cors';
 import helmet from 'helmet';
 import { schema } from './schema';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-
-const PORT = process.env.PORT;
-
 const app = express();
-app.use('*', cors() as RequestHandler);
 app.use(helmet());
 app.use(compression());
+
+if (process.env.NODE_ENV === 'development') {
+    dotenv.config({ path: '.env' });
+} else if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('public'));
+}
+
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
     schema,
