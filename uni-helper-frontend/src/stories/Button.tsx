a6e15cae4307be1b16f404143852b45b493d8bf2
Balint Dolla <balint.dolla@realcity.io>
2021-10-16 19:27:23 +0200

    Fix storybook errors

--- uni-helper-frontend/src/stories/Button.tsx
+++ uni-helper-frontend/src/stories/Button.tsx
@@ -27,22 +27,24 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
+const Button = ({
+    primary = false,
+    size = 'medium',
+    backgroundColor,
+    label,
+    ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    return (
+        <button
+            type="button"
+            className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+            style={{ backgroundColor }}
+            {...props}
+        >
+            {label}
+        </button>
+    );
 };
+
+export default Button;
