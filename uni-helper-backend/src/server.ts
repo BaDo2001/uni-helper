3f702240e7651baa44b980aac3b60d17d1151a27
Balint Dolla <balint.dolla@realcity.io>
2021-11-16 10:50:00 +0100

    Fix proxy problem + fix production webserver routing problem

--- uni-helper-backend/src/server.ts
+++ uni-helper-backend/src/server.ts
@@ -3,24 +3,13 @@ import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 import compression from 'compression';
 import dotenv from 'dotenv';
+import path from 'path';
 import { authMiddleWareExpress } from './middleware/auth';
 import schema from './graphql/schema';
 import { connectDB } from './db/db';
 
 const app = express();
 app.use(compression());
-
-if (process.env.NODE_ENV === 'development') {
-    dotenv.config({ path: '.env' });
-
-    app.get('/', (req, res) => {
-        res.sendStatus(200);
-    });
-} else if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('public'));
-}
-
-const { PORT } = process.env;
 
 const server = new ApolloServer({
     schema,
@@ -32,18 +21,32 @@ const server = new ApolloServer({
 });
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-app.use('/graphql', authMiddleWareExpress);
+app.use('/api/graphql', authMiddleWareExpress);
 
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app, path: '/api/graphql' });
+
+if (process.env.NODE_ENV === 'development') {
+    dotenv.config({ path: '.env' });
+
+    app.get('/', (req, res) => {
+        res.sendStatus(200);
+    });
+} else if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../public')));
+
+    app.get('*', (req, res) => {
+        res.sendFile('index.html', { root: path.join(__dirname, '../../public') });
+    });
+}
 
 const httpServer = createServer(app);
 
 const startServer = () => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     connectDB();
-    httpServer.listen({ port: PORT }, () => {
+    httpServer.listen({ port: process.env.PORT }, () => {
         // eslint-disable-next-line no-console
-        console.log(`\n🚀 GraphQL-Server is running on http://localhost:${PORT ?? 4000}/graphql\n`);
+        console.log(`\n🚀 GraphQL-Server is running on http://localhost:${process.env.PORT ?? 5000}/api/graphql\n`);
     });
 };
 
--- uni-helper-frontend/vite.config.ts
+++ uni-helper-frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh({
         include: ['src', 'generated-typings'],
@@ -13,7 +12,6 @@ export default defineConfig({
                 target: 'http://localhost:5000',
                 changeOrigin: true,
                 secure: false,
-                rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
     },
